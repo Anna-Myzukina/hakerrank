8877f7cbae1552932e428409b87acb8fa962a09a
Ann <anna.muzykina83@gmail.com>
2019-05-12 12:46:44 +0300

    Create DiagonalDiferences.js

---
+++ DiagonalDiferences.js
@@ -0,0 +1,49 @@
+/*
+Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+
+For example, the square matrix  is shown below:
+
+1 2 3
+4 5 6
+9 8 9  
+The left-to-right diagonal = . The right to left diagonal = . Their absolute difference is .
+
+Function description
+
+Complete the  function in the editor below. It must return an integer representing the absolute diagonal difference.
+
+diagonalDifference takes the following parameter:
+
+arr: an array of integers .
+Input Format
+
+The first line contains a single integer, , the number of rows and columns in the matrix . 
+Each of the next  lines describes a row, , and consists of  space-separated integers .
+
+*/
+
+// Complete the diagonalDifference function below.
+function diagonalDifference(arr) {
+    var LeftToRightDiagonal = [];
+    var RightToLeftDiagonal = [];
+    var sum = 0;
+
+    for (var i = 0; i < arr.length; i++){
+        for (var j = 0; j < arr.length; j--){
+            LeftToRightDiagonal = arr[0][0] + arr[1][1] + arr[2][2];
+            RightToLeftDiagonal = arr[0][2] + arr[1][1] + arr[2][0];
+            sum = LeftToRightDiagonal + RightToLeftDiagonal;
+        }
+    }
+    return sum;
+}
+/**
+ * var n = parseInt(readLine());
+var suma = 0, sumb = 0;
+for(var i = 0; i < n; i++){
+   var line = readLine().split(' ');
+   suma += Number(line[i]);
+   sumb += Number(line[n-i-1]);
+}
+console.log(Math.abs(sumb-suma));
+ */
